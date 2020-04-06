722986338b95dd5b10076104fd3cf9727286c193
Nihal Konda <nihal.konda@sjsu.edu>
2020-04-06 08:39:08 -0700

    jwt refined

--- model/jwt.js
+++ model/jwt.js
@@ -2,30 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email:{
-        type:String,
-        trim: true,
-        lowercase: true,
-        unique: true
-    },
-    firstName:{
-        type:String,
-        trim: true
-    },
-    lastName:{
-        type:String,
-        trim: true
-    },
-    phoneNumber:{
-        type:String,
-        trim: true,
-        default:''
-    },
-    displayPicture:{
-        type:String,
-        trim: true,
-        default:''
-    }
+    email:String,
+    firstName:String,
+    lastName:String,
+    phoneNumber:String,
+    displayPicture:String
 });
 
 const jwtSchema = mongoose.Schema({
